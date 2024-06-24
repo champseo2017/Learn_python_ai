a336cb44cacc8bf887b4444391c4bb63820d14c7
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-06-24 21:13:42 +0700

    เข้าถึงข้อมูลแถวเดียว เช่น train_ds[0] เราจะได้ข้อมูลออกมาในรูปแบบของ dictionary

--- myproject/main.py
+++ myproject/main.py
@@ -4,14 +4,11 @@ from datasets import load_dataset
 # โหลดชุดข้อมูล "emotion"
 emotion_dataset = load_dataset("emotion", trust_remote_code=True)
 
-# แสดงโครงสร้างข้อมูลและรายละเอียดต่างๆ
-"""
-การใช้ trust_remote_code=True จะบอกกับไลบรารีว่าคุณเชื่อถือโค้ดที่มากับชุดข้อมูลนี้และยินยอมให้มันถูกดำเนินการ.
-"""
 
 # เข้าถึงข้อมูลฝึกซ้อม (training set)
 train_ds = emotion_dataset["train"]
-print(f"Number of training samples: {len(train_ds)}")
 
-# ดูตัวอย่างข้อมูลแรกในชุดฝึก
-print(train_ds["text"][:5])
+"""
+เข้าถึงข้อมูลแถวเดียว เช่น train_ds[0] เราจะได้ข้อมูลออกมาในรูปแบบของ dictionary ที่มี key เป็นชื่อคอลัมน์ และ value เป็นข้อมูลในแถวนั้นๆ
+"""
+print(train_ds[0])
