218b357202a95d855aa0cf5943a5f93cad27d4f9
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-08-16 08:33:40 +0700

    โหลดชุดข้อมูลจาก Hugging Face

--- myproject/main.py
+++ myproject/main.py
@@ -1,18 +1,4 @@
-from transformers import AutoTokenizer, AutoModel
+from datasets import load_dataset
 
-# โหลดโมเดล BERT ที่ถูก train ด้วย bert-base-uncased tokenizer
-model = AutoModel.from_pretrained('bert-base-uncased', from_tf=True, ignore_mismatched_sizes=True)
-
-text = "Hello, how are you today?"
-
-# Case 1: ใช้ tokenizer ที่ถูกต้อง
-tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
-# โมเดลจะทำงานได้ถูกต้อง เพราะใช้ tokenizer ที่สอดคล้องกัน
-
-
-# Case 2: ใช้ tokenizer ผิด 
-wrong_tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
-encoded_input = wrong_tokenizer(text, return_tensors='pt')  
-output = model(**encoded_input)
+dataset = load_dataset("emotion")
+print(dataset)
